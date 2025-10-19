58df642125b025b39dea5fe38c0b73c9485999a3
Mumen <mumen.murad@stud.fh-campuswien.ac.at>
2025-10-19 11:26:55 +0200

    Integrate npm and a build management tool into your project.

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    root: '.',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        rollupOptions: {
+            input: {
+                main: './index.html'
+            }
+        }
+    },
+    server: {
+        port: 3000,
+        open: true
+    },
+    publicDir: 'media'
+});
