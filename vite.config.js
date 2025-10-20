b226be16e8c4e0ecf2f2224b17f28874c063e059
Mumen <mumen.murad@stud.fh-campuswien.ac.at>
2025-10-20 23:01:59 +0200

    Fixing deployment with gh-pages branch and correct base path

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,27 @@
-// vite.config.js
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
-  const repoName = 'Web-Engineering-ILV-1st-Playground';
-
-  return {
-    root: '.',
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      sourcemap: false,
-      minify: 'esbuild',
-      rollupOptions: {
-        input: {
-          main: './index.html',
-        },
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      input: {
+        main: './index.html',
       },
-      emptyOutDir: true,
     },
-    server: {
-      port: 3000,
-      open: true,
-    },
-    publicDir: 'media',
-    base: isGitHubPages ? `/${repoName}/` : '/',
-  };
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  publicDir: 'media',
+  // Important: Base path for GitHub Pages
+  base:
+    process.env.NODE_ENV === 'production'
+      ? '/Web-Engineering-ILV-1st-Playground/'
+      : '/',
 });
